81923f93d0c2cc6c6063a479efe6f59612b83042
jtliu0416 <jtliu0416@163.com>
2025-08-12 18:55:44 +0800

    test1
    test

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,7 +1,7 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteName = '蹀躞郎中'
+export const siteShortName = '蹀躞郎中'
+export const siteDescription = '蹀躞郎中的知识库'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
